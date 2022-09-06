3e706a4aaef44da06230da94030f3987e2d1b86f
Gamorr <formelakuba7@gmail.com>
2022-09-06 19:25:07 +0200

    add base

---
+++ Handman/Handman.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+void PrintMessage(string message, bool printTop = true, bool printBottom = true)
+{
+	if (printTop)
+	{
+		cout << "+---------------------------------+" << endl;
+		cout << "|";
+	}
+	bool front = true;
+	for (int i = message.length(); i < 33; i++)
+	{
+		if (front)
+		{
+			message = " " + message;
+		}
+		else
+		{
+			message = message + " ";
+		}
+		front = !front;
+	}
+	cout << message.c_str();
+	if (printBottom)
+	{
+		cout << "|" << endl;
+		cout << "+---------------------------------+" << endl;
+	}
+}
+
+int main()
+{
+	PrintMessage("HANG MAN");
+	return 0;
+}
